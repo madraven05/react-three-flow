c9f80707861cfcce32a0dd5c0ef20c03a92d5225
pranshu-kumar <pranshukumargond05@gmail.com>
2024-12-30 18:08:22 +0530

    using only html for nodes

--- src/components/three/shapes/circle.tsx
+++ src/components/three/shapes/circle.tsx
@@ -26,22 +26,9 @@ const Circle: React.FC<JSX.IntrinsicElements["group"] & CircleProps> = ({
 
   return (
     <group rotation={[-Math.PI / 2, 0, Math.PI / 2]} {...props}>
-      <lineLoop>
-        <bufferGeometry attach="geometry">
-          <bufferAttribute
-            attach="attributes-position"
-            array={new Float32Array(points.flatMap((p) => [p[0], p[1], p[2]]))}
-            count={points.length}
-            itemSize={3}
-          />
-        </bufferGeometry>
-        <lineBasicMaterial linewidth={2} color={"black"} />
-      </lineLoop>
-
       <Html transform>
         <div
-          style={{ width: `${radius * 80}px`, height: `${radius * 80}px` }}
-          className={`flex flex-col gap-5 p-14 text-center items-center justify-center rounded-full`}
+          className={`flex flex-col min-h-80 min-w-80 gap-5 p-14 border-[2px] border-black text-center items-center justify-center rounded-full`}
         >
           <h1>
             <EditableText text={title} />
--- src/components/three/shapes/rectangle.tsx
+++ src/components/three/shapes/rectangle.tsx
@@ -16,27 +16,11 @@ const Rectangle: React.FC<JSX.IntrinsicElements["group"] & RectangleProps> = ({
   description,
   ...props
 }) => {
-  const points: THREE.Vector3[] = [
-    new THREE.Vector3(-width / 2, height / 2, 0),
-    new THREE.Vector3(width / 2, height / 2, 0),
-    new THREE.Vector3(width / 2, -height / 2, 0),
-    new THREE.Vector3(-width / 2, -height / 2, 0),
-    new THREE.Vector3(-width / 2, height / 2, 0),
-  ];
-
-
   return (
     <group rotation={[-Math.PI / 2, 0, Math.PI / 2]} {...props}>
-      <Line
-        points={points}
-        color="black"
-        lineWidth={2} // Note: Line width might not work as expected in some browsers
-        dashed={false}
-      ></Line>
       <Html transform>
         <div
-          style={{ height: `${height * 40}px`, width: `${width * 40}px` }}
-          className={`flex flex-col p-8 gap-3 items-center justify-start cursor-pointer`}
+          className={`flex flex-col border-[2px] border-black p-8 gap-3 items-center justify-start cursor-pointer`}
         >
           <h1><EditableText text={title}/></h1>
           <EditableText text={description} />
